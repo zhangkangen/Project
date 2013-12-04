d725e6ba6b613b7e88682a9433a74a71acc0ba18
zhangkangen <zhangkangen@gmail.com>
2013-12-05 00:41:36 +0800

    TestAdoFail

--- testAdo/testAdo/Program.cs
+++ testAdo/testAdo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,18 @@ namespace testAdo
     {
         static void Main(string[] args)
         {
+            string dataDir = AppDomain.CurrentDomain.BaseDirectory;
+            if (dataDir.EndsWith(@"\bin\Debug\") || dataDir.EndsWith(@"\bin\Release\"))
+            {
+                dataDir = System.IO.Directory.GetParent(dataDir).Parent.Parent.FullName;
+                AppDomain.CurrentDomain.SetData("DataDirectory", dataDir);
+            }
+            using (SqlConnection conn = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDBFilename=|dataDirectoey|\Database.mfd;Integrated Security=True; Instance=True"))
+            {
+                conn.Open();
+            }
+            Console.WriteLine("成功");
+            Console.ReadKey();
         }
     }
 }
