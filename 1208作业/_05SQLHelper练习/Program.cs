c994905e4bea7dd7f74258b32f96660d46e95798
zhangkangen <zhangkangen@gmail.com>
2013-12-09 01:02:48 +0800

    homewordExtra

---
+++ 1208作业/_05SQLHelper练习/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05SQLHelper练习
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string sql = "select count(*) from tblstudent";
+			int count = (int)SQLHelper.ExecuteScalar(sql);
+			Console.WriteLine(count);
+			Console.ReadKey();
+		}
+	}
+}
