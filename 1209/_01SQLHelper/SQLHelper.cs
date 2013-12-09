52015bee390b1cd24097b92f4ba3b2263a73aaae
zhangkangen <zhangkangen@gmail.com>
2013-12-09 12:47:03 +0800

    AMTest

---
+++ 1209/_01SQLHelper/SQLHelper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace _01SQLHelper
+{
+	public class SQLHelper
+	{
+		private static readonly string strConn = ConfigurationManager.ConnectionStrings["strConn"].ConnectionString;
+		public static int ExecuteNonQuery(string sql, params SqlParameter[] param)
+		{
+			using(SqlConnection conn = new SqlConnection(strConn))
+			{
+				using(SqlCommand cmd = new SqlCommand(sql, conn))
+				{
+					conn.Open();
+					if(param != null)
+					{
+						cmd.Parameters.AddRange(param);
+					}
+					return cmd.ExecuteNonQuery();
+				}
+			}
+		}
+		public static object ExecuteScalar(string sql, params SqlParameter[] param)
+		{
+			using(SqlConnection conn = new SqlConnection(strConn))
+			{
+				using(SqlCommand cmd = new SqlCommand(sql, conn))
+				{
+					conn.Open();
+					if(param != null)
+					{
+						cmd.Parameters.AddRange(param);
+					}
+					return cmd.ExecuteScalar();
+				}
+			}
+		}
+		public static SqlDataReader ExecuteReader(string sql, params SqlParameter[] param)
+		{
+			SqlConnection conn = new SqlConnection(strConn);
+			using(SqlCommand cmd = new SqlCommand(sql, conn))
+			{
+				try
+				{
+					conn.Open();
+					if(param != null)
+					{
+						cmd.Parameters.AddRange(param);
+					}
+					return cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+				}
+				catch(Exception e)
+				{
+					conn.Close();
+					conn.Dispose();
+					throw e;
+				}
+			}
+		}
+	}
+}
---
+++ 1209/_02省市联动/Area.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02省市联动
+{
+	public class Area
+	{
+		int _areaId;
+
+		public int AreaId
+		{
+			get { return _areaId; }
+			set { _areaId = value; }
+		}
+		string _areaName;
+
+		public string AreaName
+		{
+			get { return _areaName; }
+			set { _areaName = value; }
+		}
+		int _areaPid;
+
+		public int AreaPid
+		{
+			get { return _areaPid; }
+			set { _areaPid = value; }
+		}
+		public override string ToString()
+		{
+			return this.AreaName;
+		}
+	}
+}
