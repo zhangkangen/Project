5c7b6b23a7a1a5a6625b629b37238158f862c7ee
zhangkangen <zhangkangen@gmail.com>
2013-12-13 18:28:07 +0800

    newVeryNew

---
+++ 1212/MyTryParse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTryParse
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+
+		public static bool MyTryParse(string str, out int n)
+		{
+			try
+			{
+				n = int.Parse(str);
+				return true;
+			}
+			catch
+			{
+				n = 0;
+				return false;
+			}
+		}
+	}
+}
---
+++ SQLHelper方法/SQLHelper方法/SQLHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace SQLHelper方法
+{
+	public class SQLHelper
+	{
+		public static readonly string strConn = ConfigurationManager.ConnectionStrings["strConn"].ConnectionString;
+		public static int ExecuteNonQurey(string sql, params SqlParameter[] param)
+		{
+			using(SqlConnection conn = new SqlConnection(strConn))
+			{
+				using(SqlCommand cmd = new SqlCommand(sql, conn))
+				{
+					conn.Open();
+					cmd.Parameters.AddRange(param);
+					return cmd.ExecuteNonQuery();
+				}
+			}
+		}
+
+		public static object ExecuteScalar(string sql, params SqlParameter[] param)
+		{
+			using(SqlConnection conn = new SqlConnection(strConn))
+			{
+				using(SqlCommand cmd = new SqlCommand(sql, conn))
+				{
+					conn.Open();
+					cmd.Parameters.AddRange(param);
+					return cmd.ExecuteScalar();
+				}
+			}
+		}
+		public static SqlDataReader ExecuteReader(string sql, params SqlParameter[] param)
+		{
+			SqlConnection conn = new SqlConnection(strConn);
+			using(SqlCommand cmd = new SqlCommand(sql, conn))
+			{
+				try
+				{
+					conn.Open();
+					cmd.Parameters.AddRange(param);
+					return cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+				}
+				catch(Exception e)
+				{
+					conn.Close();
+					conn.Dispose();
+					throw e;
+				}
+			}
+		}
+		public static DataTable GetDataTable(string sql)
+		{
+			DataTable dt = new DataTable();
+			using(SqlDataAdapter sda = new SqlDataAdapter(sql, strConn))
+			{
+				sda.Fill(dt);
+			}
+			return dt;
+		}
+
+	}
+}
