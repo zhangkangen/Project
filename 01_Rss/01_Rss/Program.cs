f4cb9f1f0f38c9aafcbf838d2b87ac9d08f88f6c
zhangkangen <zhangkangen@gmail.com>
2013-12-15 19:54:14 +0800

    HtmlStudyAndCss

---
+++ 01_Rss/01_Rss/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Feeds;
+using Microsoft.Feeds.Interop;
+
+namespace _01_Rss
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			FeedsManagerClass fm = new FeedsManagerClass();
+			string url = @"http://news.163.com/special/00011K6L/rss_newstop.xml"; //URL
+			IFeedFolder rootFolder = (IFeedFolder)fm.RootFolder;
+			IFeed feed = null;
+			if(!fm.IsSubscribed(url))
+			{
+				feed = (IFeed)rootFolder.CreateFeed(url, url);
+				feed.Rename("Nord Pool Spot UMM");
+			}
+			else
+			{
+				feed = (IFeed)fm.GetFeed(@"Nord Pool Spot UMM");
+			}
+			feed.Download();
+
+			IFeedItem tt = feed.GetItem(0) as IFeedItem;
+			string title = tt.Title;
+
+			string xml = feed.Xml(feed.ItemCount,
+				FEEDS_XML_SORT_PROPERTY.FXSP_PUBDATE,
+				FEEDS_XML_SORT_ORDER.FXSO_ASCENDING,
+				FEEDS_XML_FILTER_FLAGS.FXFF_ALL,
+				FEEDS_XML_INCLUDE_FLAGS.FXIF_CF_EXTENSIONS);
+			StreamWriter sw = new StreamWriter(File.OpenWrite("test.xml"));
+			sw.Write(xml);
+			sw.Close();
+		}
+	}
+}
---
+++ _01html/_01html/JScript/_1AlertTime.js
@@ -0,0 +1,1 @@
+﻿alert(new Date().toLocaleDateString() + new Date().toLocaleTimeString());
